409c970137b3082265e511870d9352207f16dd76
Juan Alvarez <jfhack7@gmail.com>
2018-01-11 03:29:02 -0300

    upload files using form multipart in one request + 2 test inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component,OnInit, OnDestroy  } from '@angular/core';
 
-import {FormControl, Validators} from '@angular/forms';
-import {Form1}  from './FormData';
+import { FormControl, Validators } from '@angular/forms';
+import { Form1}  from './FormData';
 import { ConsumeRestAPIService } from './app.services';
 //import {AppService}  from './app.services';
+
+interface MyForm {
+  name?: string;
+  uinnumber?: string;
+  mailingaddress?: string;
+  email?: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -13,27 +20,66 @@ import { ConsumeRestAPIService } from './app.services';
 })
 export class AppComponent {
   title = 'app';
-  //public form1:Form1;
-  name:string='';
-  uinnumber:string='';
-  mailingaddress:string='';
-  emailaddress:string='';
+  form1: MyForm = {};
   
   constructor(
     private _consumeRestAPIService: ConsumeRestAPIService
   ) {}
   
   email = new FormControl('', [Validators.required, Validators.email]);
+  filesDict = {};
   
-    getErrorMessage() {
-      return this.email.hasError('required') ? 'You must enter a value' :
-          this.email.hasError('email') ? 'Not a valid email' :
-              '';
-    }
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
+  }
 
-    onSubmit(){
-      //console.log("asdasdasd"+this.name+this.femail)
-      this._consumeRestAPIService.SenDataForm(this.name,this.uinnumber,this.mailingaddress,this.emailaddress).subscribe(response =>{response.data;console.log("test")});
-      
+
+  onFileInput($event, name){
+    if($event.target.files && $event.target.files.length > 0){
+      this.filesDict[name] = $event.target.files[0];
+    }else{
+      delete this.filesDict[name];
     }
+  }
+
+  onSubmit(){
+    //console.log("asdasdasd"+this.name+this.femail)
+    //this._consumeRestAPIService.SenDataForm(this.name,this.uinnumber,this.mailingaddress,this.emailaddress).subscribe(response =>{response.data;console.log("test")});
+    let formData: FormData = new FormData();
+    var i;
+    for(i in this.form1){
+      formData.append(i, this.form1[i]);
+    };
+
+    for(i in this.filesDict){
+      if(this.filesDict[i]){
+        formData.append(i, this.filesDict[i]);
+      }
+    };
+    
+    let xhr: XMLHttpRequest = new XMLHttpRequest();
+    xhr.open('POST', "http://ceinina.com/FormAHS/test_sqlite.php", true);
+
+    // enctype For Multipart Request
+    xhr.setRequestHeader("enctype", "multipart/form-data");
+
+    // IE bug fixes to clear cache
+    xhr.setRequestHeader("Cache-Control", "no-cache");
+    xhr.setRequestHeader("Cache-Control", "no-store");
+    xhr.setRequestHeader("Pragma", "no-cache"); 
+
+    xhr.send(formData);
+    
+    xhr.onreadystatechange = () => {
+      if(xhr.readyState === 4) {
+        if(xhr.status === 201) {
+            console.log("Success");
+        } else {
+            console.log("Error");
+        }
+      }
+    }
+  }
 }
